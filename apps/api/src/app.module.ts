21230f448a8b013093e0b18890b70a8015ad2b63
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-06-20 15:53:12 -0600

    Añadir configuraciones funcionales

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,16 @@
 import { Module } from "@nestjs/common";
 import { ConfigModule } from "@nestjs/config";
 import { LtiModule } from "./controllers/lti/lti.module";
+import { ServeStaticModule } from "@nestjs/serve-static";
+import { join } from "path";
 
 @Module({
-    imports: [LtiModule, ConfigModule.forRoot({ isGlobal: true, cache: true })],
+    imports: [
+        LtiModule,
+        ConfigModule.forRoot({ isGlobal: true, cache: true }),
+        ServeStaticModule.forRoot({
+            rootPath: join(__dirname, "../..", "client", "dist"),
+        }),
+    ],
 })
 export class AppModule {}
