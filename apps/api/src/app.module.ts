a3eeef6565e65b4f8d16b67cad182a295108bae7
Ian Ramirez-España <ianespana@users.noreply.github.com>
2023-06-20 20:47:24 -0600

    Usar Mongoose

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from "@nestjs/common";
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule, ConfigService } from "@nestjs/config";
 import { LtiModule } from "./modules/lti/lti.module";
 import { ServeStaticModule } from "@nestjs/serve-static";
 import { join } from "path";
+import { MongooseModule } from "@nestjs/mongoose";
 
 @Module({
     imports: [
@@ -11,6 +12,13 @@ import { join } from "path";
         ServeStaticModule.forRoot({
             rootPath: join(__dirname, "../..", "client", "dist"),
             exclude: ["/api/(.*)"],
+        }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            useFactory: async (configService: ConfigService) => ({
+                uri: configService.get("MONGO_URI", ""),
+            }),
+            inject: [ConfigService],
         }),
     ],
 })
--- apps/api/src/modules/lti/lti.service.ts
+++ apps/api/src/modules/lti/lti.service.ts
@@ -9,52 +9,11 @@ import { LtiaasService } from "../ltiaas/ltiaas.service";
 
 @Injectable()
 export class LtiService {
-    private client: MongoClient;
-    private db: Db;
-
     constructor(
         private readonly configService: ConfigService,
         private readonly httpService: HttpService,
         private readonly litaasService: LtiaasService,
-    ) {
-        const uri = "mongodb+srv://admin:btdyw7rgG6mpYCft@cluster0.4twbbsi.mongodb.net/?retryWrites=true&w=majority";
-        this.client = new MongoClient(uri, {
-            serverApi: {
-                version: "1",
-            },
-        });
-    }
-
-    async connect(): Promise<void> {
-        try {
-            await this.client.connect();
-            this.db = this.client.db("GitHubLinks");
-            console.log("Conexión a la base de datos establecida");
-        } catch (error) {
-            console.error("Error al conectar a la base de datos:", error);
-        }
-    }
-
-    async disconnect(): Promise<void> {
-        try {
-            await this.client.close();
-            console.log("Desconexión de la base de datos exitosa");
-        } catch (error) {
-            console.error("Error al desconectar de la base de datos:", error);
-        }
-    }
-
-    async getRepositoryUrls(): Promise<string[]> {
-        try {
-            const collection = this.db.collection("GitHubLinks");
-            const result = await collection.find().toArray();
-            const urls = result.map((item) => item.url);
-            return urls;
-        } catch (error) {
-            console.error("Error al obtener los URLs de GitHub:", error);
-            return [];
-        }
-    }
+    ) {}
 
     async handleLaunchRequest(launch: LtiaasCallbackDto) {
         return await this.litaasService.getIdToken(launch.ltik);
